af991c540bf44c2597181dfe47aa41e57cd78fd3
Nakamon Tongyuen <nakamon@infinitylevels.com>
2017-05-25 11:44:30 +0700

    fix files ignored by gitignore

--- cocos2dx/platform/third_party/android/prebuilt/zlib/Android.mk
+++ cocos2dx/platform/third_party/android/prebuilt/zlib/Android.mk
@@ -3,6 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_zlib_static
 LOCAL_MODULE_FILENAME := zlib
-LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libz.a
+LOCAL_SRC_FILES := libfiles/$(TARGET_ARCH_ABI)/libz.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(PREBUILT_STATIC_LIBRARY)
